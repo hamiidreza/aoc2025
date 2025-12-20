7b6bbe498eb58e755b6961a75b8cf4020f6f79ee
Hamidreza <hamid.khoshakhlagh@partisia.com>
2025-12-20 19:52:54 +0330

    day 7, part 1 done

--- src/bin/day071.rs
+++ src/bin/day071.rs
@@ -1,13 +1,20 @@
 use std::{collections::HashSet, fs};
 
 fn beam(grid: &Vec<Vec<char>>, x: usize, y: usize, visited: &mut HashSet<(usize, usize)>) {
-    for i in x..grid.len() - 1 {
-        if grid[i + 1][y] == '^' && visited.insert((i + 1, y)) {
-            visited.insert((i + 1, y));
-            beam(&grid, i + 1, y - 1, visited);
-            beam(&grid, i + 1, y + 1, visited);
-            break;
+    if x >= grid.len() || y >= grid[0].len() {
+        return;
+    }
+    match grid[x][y] {
+        '.' => {
+            beam(grid, x + 1, y, visited);
         }
+        '^' => {
+            if visited.insert((x, y)) {
+                beam(grid, x + 1, y - 1, visited);
+                beam(grid, x + 1, y + 1, visited);
+            }
+        }
+        _ => {}
     }
 }
 
@@ -15,12 +22,7 @@ fn main() {
     let input = fs::read_to_string("inputs/day07.txt").expect("Failed to read input file");
     let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
     let mut visited_positions: HashSet<(usize, usize)> = HashSet::new();
-    for y in 0..grid[0].len() {
-        if grid[0][y] == 'S' {
-            beam(&grid, 1, y, &mut visited_positions);
-            break;
-        }
-    }
-
+    let start_y = grid[0].iter().position(|&c| c == 'S').unwrap();
+    beam(&grid, 1, start_y, &mut visited_positions);
     println!("{:?}", visited_positions.len());
 }
